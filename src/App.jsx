5188eaf00a547db95d2077b4e8fa1f36096c4e16
awat <154649552+Away12568@users.noreply.github.com>
2024-11-29 19:08:55 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import './App.css'
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/footer/Footer'
 import Header from './components/Header/Header'
 import Shop from './pages/Shop'
-import Footer from './components/footer/Footer'
+import Product from './pages/Product'
 
 const App = () => {
   return (
     <>
-       <Header/>
-
+     <Header />
     <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/shop' element={<Shop/>} />
+    <Route path='/product/:id' element={<Product/>}/>
     </Routes>
-
-      <Footer/>
+    <Footer/>
     </>
   )
 }
